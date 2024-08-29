86d496a56369b4e9a4e9c709453fc0f84c439a63
Anwar Ramadha <anwarramadha22@gmail.com>
2024-08-29 23:10:15 +0700

    fix: :bug: removing await in md class

--- src/lib/md.ts
+++ src/lib/md.ts
@@ -13,14 +13,14 @@ export class Md {
 
   downloadUrlAndReplaceUrls(): void {
     const urls = this.extractUrls();
-    urls.forEach(async (url) => {
+    urls.forEach((url) => {
       //remove ) from the end of the url
       if (url.endsWith(')')) {
         url = url.slice(0, -1);
       }
       try {
         const urlInstance = new URL(url);
-        await download(urlInstance.pathname);
+        download(urlInstance.pathname);
         // if file is downloaded, replace the url with the local path
         const host = import.meta.env.ASTRO_SITE_URL;
         this.text = this.text.replace(url, `${host}/${process.env.ASTRO_BASE_PATH}/uploads/${url.split("/").pop()}`);
