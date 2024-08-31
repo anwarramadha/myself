207c10dada7a6b6e04cfc3090736e74ef573f5fa
Anwar Ramadha <anwarramadha22@gmail.com>
2024-08-31 22:55:10 +0700

    feat: :sparkles: added custom head image

--- src/interfaces/siteInfo.ts
+++ src/interfaces/siteInfo.ts
@@ -1,8 +1,11 @@
+import type Image from './image/image'
+
 export default interface SiteInfo {
   id: number;
   attributes: {
     title: string;
     description: string;
+    image: Image;
     createdAt: string;
     updatedAt: string;
   }
