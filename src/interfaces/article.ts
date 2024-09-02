fe2424fc8b4a83999ee7687bbbc5a98caa5d7efd
Anwar Ramadha <anwarramadha22@gmail.com>
2024-09-02 09:49:17 +0700

    feat: :sparkles: added article hero image & reading time (#13)
    * feat: :sparkles: added article hero image & reading time
    
    * fix: :bug: fixed undefined read attribute while opening blog

--- src/interfaces/article.ts
+++ src/interfaces/article.ts
@@ -1,9 +1,12 @@
 import type Tag from "./tag";
 import type Stack from "./stack";
+import type Image from "./image/image";
 
 export default interface Article {
+  heroImage: Image;
   summary: string;
   content: string;
+  readingTime: number;
   createdAt: string;
   updatedAt: string;
   tags: {
